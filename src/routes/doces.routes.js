b2eaba6cdd78bfdcd0168f5af7ae36d3a34c7d31
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-09-05 11:15:08 -0300

    feat: create put method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -4,7 +4,7 @@ const docesRoutes = Router()
 
 const guloseimas = [
     {
-        id: 1, 
+        id: 1,
         nome: "Trufa",
         preco: 8.50,
     },
@@ -21,13 +21,13 @@ const guloseimas = [
 ]
 
 //Rota para buscar todos os elementos do array guloseimas
-docesRoutes.get("/", (req,res) => {
+docesRoutes.get("/", (req, res) => {
     return res.status(200).send(guloseimas)
 })
 
 //Rota para criar nova guloseima
-docesRoutes.post("/", (req,res) => {
-    const{nome, preco} = req.body
+docesRoutes.post("/", (req, res) => {
+    const { nome, preco } = req.body
 
     const novoDoce = {
         id: guloseimas.length + 1,
@@ -41,21 +41,46 @@ docesRoutes.post("/", (req,res) => {
 })
 
 //Rota para buscar um elemento específico do array guloseimas
-docesRoutes.get("/:id", (req,res) => {
+docesRoutes.get("/:id", (req, res) => {
     const { id } = req.params
-    
+
 
     //console.log(id)
 
     const guloseima = guloseimas.find((doce) => doce.id === Number(id))
 
-    console.log(guloseima)
+    //console.log(guloseima)
 
-    if(!guloseima){
-        return res.status(404).send({message: "Guloseima não encontrada!"})
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!" })
     }
 
     return res.status(200).send(guloseima)
+})
+
+//Rota para editar uma guloseima
+docesRoutes.put("/:id", (req, res) => {
+    const { id } = req.params
+
+    const guloseima = guloseimas.find((doce) => doce.id === Number(id))
+
+    //console.log(guloseima)
+
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!" })
+    }
+
+    const { nome, preco } = req.body
+
+    console.log(nome)
+
+    guloseima.nome = nome
+    guloseima.preco = preco
+
+    return res.status(200).send({
+        message: "Guloseima atualizada!",
+        guloseima,
+    })
 })
 
 export default docesRoutes
--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(routes)
-
 app.use(express.json())
+
+app.use(routes)
 
 app.listen(port, () =>{
     console.log(`🌷 Server started on http://localhost:${port}`)
