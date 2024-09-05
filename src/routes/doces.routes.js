3165fa5c364ad4e70d953830656dd7167e357338
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-09-05 09:52:44 -0300

    feat: create get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -20,10 +20,12 @@ const guloseimas = [
     }
 ]
 
+//Rota para buscar todos os elementos do array guloseimas
 docesRoutes.get("/", (req,res) => {
     return res.status(200).send(guloseimas)
 })
 
+//Rota para criar nova guloseima
 docesRoutes.post("/", (req,res) => {
     const{nome, preco} = req.body
 
@@ -36,6 +38,24 @@ docesRoutes.post("/", (req,res) => {
     guloseimas.push(novoDoce)
 
     return res.status(201).send(guloseimas)
+})
+
+//Rota para buscar um elemento específico do array guloseimas
+docesRoutes.get("/:id", (req,res) => {
+    const { id } = req.params
+    
+
+    //console.log(id)
+
+    const guloseima = guloseimas.find((doce) => doce.id === Number(id))
+
+    console.log(guloseima)
+
+    if(!guloseima){
+        return res.status(404).send({message: "Guloseima não encontrada!"})
+    }
+
+    return res.status(200).send(guloseima)
 })
 
 export default docesRoutes
